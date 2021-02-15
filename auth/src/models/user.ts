a79e8d502ad8142a5aa52c0468245c7a3aa18b48
StephSince93 <stephen@stemsoftware.com>
2021-02-15 13:22:00 -0600

    Set up mongoose user sign-up model and implemented into signup component, created bad request error component

---
+++ auth/src/models/user.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+// An interface that describes the poperties
+// that are required to create a new user
+interface UserAttrs {
+    email: string
+    password: string
+}
+
+// An interface that describes the properties 
+// that the User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc
+}
+
+// An interface that describes the properties 
+// that the User Document has
+interface UserDoc extends mongoose.Document {
+    email: string
+    password: string
+}
+
+const userSchema = new mongoose.Schema<UserDoc>({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// Create new build method inside of schema
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs)
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
+
+
+export { User }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response }  from 'express'
 import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
 
+import { User } from '../models/user'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { BadRequestError } from '../errors/bad-request-error'
 const router = express.Router()
 
 router.post('/api/users/signup', 
@@ -22,12 +23,19 @@ router.post('/api/users/signup',
         if(!errors.isEmpty()) {
             throw new RequestValidationError(errors.array())
         }
-
         const { email, password } = req.body
-    
-        throw new DatabaseConnectionError()
 
-        res.send({})
-})
+        const existingUser = await User.findOne({email})
+
+        if(existingUser) {
+            throw new BadRequestError('Email in use')
+        }
+
+        const user = User.build({ email, password})
+        await user.save()
+
+        res.status(201).send(user)
+    }
+)
 
 export { router as signupRouter}
