3cdaf01934e961ead49e87a90ef7a9a02ad49dba
StephSince93 <stephen@stemsoftware.com>
2021-02-17 13:01:37 -0600

    tweaked signout component, added middleware for current-user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,22 +1,14 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 
+import { currentUser } from '../middlewares/current-user'
+
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req,res) => {
-    if (!req.session?.jwt) { // equivalent to if (!req.session || req.session.jwt)
-        return res.send({ currentuser: null })
-    }
-    try{
-        const payload = jwt.verify(
-            req.session.jwt, 
-            process.env.JTW_KEY!)
+router.get('/api/users/currentuser', currentUser, (req,res) => {
+   
+    res.send({ currentUser: req.currentUser  || null })   
 
-        res.send({ currentUser: payload })   
-        
-    }catch(err) {
-        res.send({currentUser: null })
-    }
 })
 
 export { router as currentUserRouter }
